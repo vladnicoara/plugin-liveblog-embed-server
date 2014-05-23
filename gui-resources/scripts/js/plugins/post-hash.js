c4e7034f8272ad091087fa504d54c0f458cb941c
vladnicoara <vlad.nicoara@sourcefabric.org>
2014-05-23 17:32:45 +0300

    Update post-hash.js

--- gui-resources/scripts/js/plugins/post-hash.js
+++ gui-resources/scripts/js/plugins/post-hash.js
@@ -29,7 +29,7 @@ define([
                 if (href.indexOf(_hashMark) === -1) {
                     permalink = href + _hashMark + newHash;
                 } else if (href.indexOf(hash + '=') !== -1) {
-                    var regexHash = new RegExp(hash + '=[^&]*');
+                    var regexHash = new RegExp(hash + '=[^&#]*');
                     permalink = href.replace(regexHash, newHash);
                 } else {
                     permalink = href + '&' + newHash;
